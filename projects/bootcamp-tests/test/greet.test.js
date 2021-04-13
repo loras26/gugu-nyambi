ef05d543001c069659a8d71cfb3b6a7a792b906c
loras26 <nyambegugu@gmail.com>
2021-04-13 15:27:09 +0200

    the greet function

---
+++ projects/bootcamp-tests/test/greet.test.js
@@ -0,0 +1,8 @@
+describe('the test for the greet function' , function(){
+    it('this function should return : "Hello, pupu"' , function(){
+        assert.equal("Hello, pupu", greet("pupu"));
+
+        //assert.deepEqual([2,2],[2,2]);
+    });
+
+});
